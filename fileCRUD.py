a7b9016b80094b6c24694c5ffad2d9cae7f59bd2
danvelescu <velescudan98@gmail.com>
2020-12-12 11:22:16 +0200

    Commit for impact finalPrg122

--- fileCRUD.py
+++ fileCRUD.py
@@ -1,10 +1,12 @@
+import json
+
 class Product:
-    # def __init__(self, name, price, color):
-    #     self.Name = name
-    #     self.Price = price
-    #     self.Color = color
-    #     if (name == '' and price == '' and color == ''):
-    #         self.SaveProduct()
+    def __init__(self, name, price, color):
+        self.Name = name
+        self.Price = price
+        self.Color = color
+        if (name == '' and price == '' and color == ''):
+            self.SaveProduct()
 
     def SaveProduct(self):
         self.Name = input('Введите название продукта ')
@@ -15,27 +17,62 @@ class Product:
             self.SaveProduct()
         self.Color = input('Введите цвет продукта')
         fileRessurce = open("NameOfProducts.txt", "a")
-        fileRessurce.write('\n{"name":"'+self.Name+'",'+'"price":"'+str(self.Price)+'",'+'"color":"'+str(self.Color)+'"}')
+        fileRessurce.write('{"name":"'+self.Name+'",'+'"price":"'+str(self.Price)+'",'+'"color":"'+str(self.Color)+'"}\n')
         fileRessurce.close()
 
-    # def deleteAll(self):
-
-x = '{"name":"dd","price":123,"color":"sd"}'
-
-
-
-
-product1 = Product()
-product1.SaveProduct()
+    def deleteAll(self):
+        try:
+            fileRessurse = open("NameOfProducts.txt","w")
+        except:
+            print("file not exists!!!")
+        fileRessurse.write("")
+        fileRessurse.close()
 
 
-import json
+    def jsonToProduct(self):
+        fileResource = open("NameOfProducts.txt","r")
+        products = []
+        for product in fileResource:
+            p = json.loads(product)
+            products.append(Product(p['name'],p['price'],p['color']))
+        fileResource.close()
+        return products
 
 
 
+    def printMyProduct(self):
+        for product in self.jsonToProduct():
+            print(product.Color)
+
+    def summpriceProducts(self):
+        products = self.jsonToProduct()
+        summ = 0
+        for product in products:
+            summ += int(product.Price)
+        print(summ)
+
+
+
+
+
+
+
+
+
+x = '{"name":"sadas","price":"123","color":"qwe"}'
 
 eu = json.loads(x)
 
-print(eu['name'])
+print(eu['color'])
+
+
+product1 = Product("1","1","1")
+
+product1.summpriceProducts()
+
+
+
+
+
 
 
