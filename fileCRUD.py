50f24d3cd0f75321556435a6a31174d338099b81
danvelescu <velescudan98@gmail.com>
2020-12-12 10:17:02 +0200

    Commit for impact finalPrg12

--- fileCRUD.py
+++ fileCRUD.py
@@ -1,10 +1,10 @@
 class Product:
-    def __init__(self, name, price, color):
-        self.Name = name
-        self.Price = price
-        self.Color = color
-        if (name == '' and price == '' and color == ''):
-            self.SaveProduct()
+    # def __init__(self, name, price, color):
+    #     self.Name = name
+    #     self.Price = price
+    #     self.Color = color
+    #     if (name == '' and price == '' and color == ''):
+    #         self.SaveProduct()
 
     def SaveProduct(self):
         self.Name = input('Введите название продукта ')
@@ -15,8 +15,27 @@ class Product:
             self.SaveProduct()
         self.Color = input('Введите цвет продукта')
         fileRessurce = open("NameOfProducts.txt", "a")
-        fileRessurce.write("{" + "'name':" + self.Name + ",'price':" + str(self.Price) + ",'color':" + self.Color + "}")
+        fileRessurce.write('\n{"name":"'+self.Name+'",'+'"price":"'+str(self.Price)+'",'+'"color":"'+str(self.Color)+'"}')
         fileRessurce.close()
 
+    # def deleteAll(self):
 
-product1 = Product('asd', 2, "22")
+x = '{"name":"dd","price":123,"color":"sd"}'
+
+
+
+
+product1 = Product()
+product1.SaveProduct()
+
+
+import json
+
+
+
+
+eu = json.loads(x)
+
+print(eu['name'])
+
+
